d4ebe09e8b16f938273e561d9081057c30d8155d
deannapi <deannapi.mart@gmail.com>
2020-08-02 21:17:43 -0500

    added header and footer info

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,34 @@
 import React from "react";
-
-class Footer extends React.Component {
+ 
+export default class Footer extends React.Component {
     render() {
+        let resumeInfo = this.props.resumeInfo;
         return (
-            <div id="footer" className="container-fluid text-center">
-                <div className="contact-grid">
-                    <h4>Follow Me:</h4>
-                    <ul className="a">
-                        <li className="github">
-                        <a href="https://github.com/deannapi"> 
-                            <i className="fab fa-github"></i>
-                        </a>
-                        </li>
-                        <li className="instagram">
-                        <a href="http://www.instagram.com/deannapi">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                        </li>
-                        <li className="linkedin">
-                        <a href="https://www.linkedin.com/in/deanna-martinez-63539a137/">
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                        </li>
-                    </ul>
-                    <h5 className="footer-name">DeAnna Martinez &copy; 2020</h5>
+            <footer>
+                <div id="footer" className="container-fluid text-center">
+                    <div className="contact-grid">
+                        <h4>Follow Me:</h4>
+                        <ul className="a">
+                            <li className="github">
+                            <a href="https://github.com/deannapi"> 
+                                <i className="fab fa-github"></i>
+                            </a>
+                            </li>
+                            <li className="instagram">
+                            <a href="http://www.instagram.com/deannapi">
+                                <i className="fab fa-instagram"></i>
+                            </a>
+                            </li>
+                            <li className="linkedin">
+                            <a href="https://www.linkedin.com/in/deanna-martinez-63539a137/">
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                            </li>
+                        </ul>
+                        <h5 className="footer-name">DeAnna Martinez &copy; 2020</h5>
+                    </div>
                 </div>
-            </div>
-        )
+            </footer>
+        );
     }
 }
-
-export default Footer;
