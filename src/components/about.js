92ab6af9891bb2f7beb310f487ff44d5787f0028
deannapi <deannapi.mart@gmail.com>
2020-08-02 21:06:34 -0500

    redid about component

--- src/components/about.js
+++ src/components/about.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import selfie from "../img/selfie.JPG";
-// import PropTypes from "prop-types";
-
-function About() {
+import React, { Component } from "react";
+export default class About extends Component {
+  render() {
+    let resumeInfo = this.props.resumeInfo;
     return (
-        <div className="grid-container">
-            <div className="grid-about">
-                <section className="my-5">
-                    <img src={selfie} className="my-2" style={{ width: "15%" }} alt="selfie" id="selfie"></img>
-                    <p className={"myText" + this.props.pulse}>
-                        I have a Master's of Arts degree in Mathematics and a solid knowledge for complex
-                        Data Analysis. Having excelled in creating data driven reports using existing and emerging
-                        software tools, I have continued to refine my strengths in information technology tools, 
-                        object-oriented programming, data interpretation and problem solving. Adding a Full Stack 
-                        Web Developer certification will allow me to utilize my skills and contribute in my career 
-                        as a developer. </p>
-                </section>
+      <section id="about">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src="images/selfie.jpg" alt="selfie" />
+          </div>
+
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>{resumeInfo.aboutme}</p>
+
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Me</h2>
+                <p className="address">
+                  <span>{resumeInfo.name}</span>
+                  <br></br>
+                  <span>{resumeInfo.address}</span>
+                  <br></br>
+                  <span>{resumeInfo.website}</span>
+                </p>
+              </div>
             </div>
+          </div>
         </div>
-        
+      </section>
     );
+  }
 }
-
-export default About;
-
-// About.propTypes = {
-//     pulse: PropTypes.string
-// };
