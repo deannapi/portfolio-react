9c362c1c540bd0a33610cd01b8848e476d2a74ed
deannapi <deannapi.mart@gmail.com>
2020-08-06 19:20:48 -0500

    Updates

---
+++ precache-manifest.cc3126c7e713cc606dbf5120605dc499.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b756fcb8466c8f9f6f82f63a43337ac",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "9af2734da218e91907a5",
+    "url": "/portfolio-react/static/css/main.53b42851.chunk.css"
+  },
+  {
+    "revision": "a7a1775afc64905f6fe7",
+    "url": "/portfolio-react/static/js/2.1903367c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.1903367c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af2734da218e91907a5",
+    "url": "/portfolio-react/static/js/main.b41d7cdd.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "1ab9ce15af65ff64f0c11bd506225533",
+    "url": "/portfolio-react/static/media/book_movie.1ab9ce15.jpg"
+  },
+  {
+    "revision": "bf81c960a9b9ff917544134a242019a9",
+    "url": "/portfolio-react/static/media/budget.bf81c960.jpg"
+  },
+  {
+    "revision": "787166f2b4547ad532cab81f0386c7ff",
+    "url": "/portfolio-react/static/media/food_fest.787166f2.jpg"
+  },
+  {
+    "revision": "53325b8f917ec494ca652902aa80e0f4",
+    "url": "/portfolio-react/static/media/motor.53325b8f.jpg"
+  },
+  {
+    "revision": "e89718f0dbdf587538600233afebc971",
+    "url": "/portfolio-react/static/media/note_taker.e89718f0.jpg"
+  },
+  {
+    "revision": "59b88d534e14111ad18f4609be370157",
+    "url": "/portfolio-react/static/media/pswd_gen.59b88d53.jpg"
+  },
+  {
+    "revision": "e78f5505d334f1c1c854cb88e4761860",
+    "url": "/portfolio-react/static/media/selfie.e78f5505.JPG"
+  },
+  {
+    "revision": "acb71e0f82f275957f59f032221e25d1",
+    "url": "/portfolio-react/static/media/stack-overflow.acb71e0f.png"
+  },
+  {
+    "revision": "6135a4ffc301956a43be3f42db89f35f",
+    "url": "/portfolio-react/static/media/weather.6135a4ff.jpg"
+  },
+  {
+    "revision": "b9a3791ab3467b72d50314dd67a28c47",
+    "url": "/portfolio-react/static/media/workday.b9a3791a.jpg"
+  }
+]);
