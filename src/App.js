6538a1501ed9a78583fedb6b308283aa8a4e1a9f
deannapi <deannapi.mart@gmail.com>
2020-08-06 20:37:03 -0500

    deploying again

--- src/App.js
+++ src/App.js
@@ -5,25 +5,7 @@ import Resume from "./components/resume";
 import Portfolio from "./components/portfolio";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
-// import resumeInfo from "./resumeInfo";
 import "./App.css";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Header resumeData={resumeInfo} />
-//         <About resumeData={resumeInfo} />
-//         <Resume resumeData={resumeInfo} />
-//         <Portfolio resumeData={resumeInfo} />
-//         <Contact resumeData={resumeInfo} />
-//         <Footer resumeData={resumeInfo} />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 function App() {
   const [ResumeSelected, setResumeSelected] = useState(false);
