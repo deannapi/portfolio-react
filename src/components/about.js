b282e6e0702129bc2263f54b76a500ff8f180b19
deannapi <deannapi.mart@gmail.com>
2020-08-24 14:34:45 -0500

    redid brand statement

--- src/components/about.js
+++ src/components/about.js
@@ -2,18 +2,22 @@ import React from "react";
 import selfie from "../img/selfie.JPG";
 
 export default function About() {
-  return(
+  return (
     <section className="about" id="about">
-        <h3>About Me</h3>
-        <div className="about-me">
-          <img
-            className="profile-pic"
-            src={selfie}
-            alt="selfie"
-            height="220px"
-          />
-          <div>
-            <p>
+      <h3>I am DeAnna Martinez</h3>
+      <div className="about-me">
+        <img className="profile-pic" src={selfie} alt="selfie" height="220px" />
+        <div>
+          <p>
+            A Full Stack Web Developer with a
+            certification from The University of Texas at Austin. I am an
+            experienced Data Analyst with a Master's of Arts degree in
+            Mathematics. Equipped with a diverse skill-set and proven ability to
+            deliver desired results. I am accomplished in the development of
+            algorithms, problem solving and building responsive websites and
+            applications.
+          </p>
+          {/* <p>
               I have a Master's of Arts degree in Mathematics and a solid
               knowledge for complex Data Analysis. Having excelled in creating
               data driven reports using existing and emerging software tools, I
@@ -22,9 +26,9 @@ export default function About() {
               problem solving. Adding a Full Stack Web Developer certification
               will allow me to utilize my skills and contribute in my career as
               a developer.
-            </p>
-          </div>
+            </p> */}
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
