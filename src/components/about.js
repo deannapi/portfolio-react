57d6438a72766ba84c68876badab9c501f5ca6b8
deannapi <deannapi.mart@gmail.com>
2020-09-10 07:29:30 -0500

    changed wording

--- src/components/about.js
+++ src/components/about.js
@@ -10,23 +10,13 @@ export default function About() {
         <div>
           <p>
             A Full Stack Web Developer with a
-            certification from The University of Texas at Austin. I am an
+            certificate from The University of Texas at Austin. I am an
             experienced Data Analyst with a Master's of Arts degree in
             Mathematics. Equipped with a diverse skill-set and proven ability to
             deliver desired results. I am accomplished in the development of
             algorithms, problem solving and building responsive websites and
             applications.
           </p>
-          {/* <p>
-              I have a Master's of Arts degree in Mathematics and a solid
-              knowledge for complex Data Analysis. Having excelled in creating
-              data driven reports using existing and emerging software tools, I
-              have continued to refine my strengths in information technology
-              tools, object-oriented programming, data interpretation and
-              problem solving. Adding a Full Stack Web Developer certification
-              will allow me to utilize my skills and contribute in my career as
-              a developer.
-            </p> */}
         </div>
       </div>
     </section>
