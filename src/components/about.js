a154a7f1e4293418e74c70f0e760f230408a0d3a
deannapi <deannapi.mart@gmail.com>
2020-10-28 14:50:35 -0500

    changed about me

--- src/components/about.js
+++ src/components/about.js
@@ -9,14 +9,17 @@ export default function About() {
         <img className="profile-pic" src={selfie} alt="selfie" height="220px" />
         <div>
           <p>
-            A Full Stack Web Developer with a
-            certificate from The University of Texas at Austin. I am an
-            experienced Data Analyst with a Master's of Arts degree in
-            Mathematics. Equipped with a diverse skill-set and proven ability to
-            deliver desired results. I am accomplished in the development of
-            algorithms, problem solving and building responsive websites and
-            applications. With a strong knowledge of HTML, CSS and creating back-end connections,
-            I am confident that I will fulfill the duties and skills required for a developer role.
+            Motivated Full Stack Web Developer with a strong background in
+            mathematics and data analytics. Passionate about building
+            first-class web applications, writing and testing code,
+            troubleshooting issues and implementing new techniques. Keen
+            interest in object-oriented programming and software development.
+            Equipped with a diverse skill-set and proven ability to deliver
+            desired results. I am accomplished in the development of algorithms,
+            problem solving and building responsive websites and applications.
+            With a strong knowledge of HTML, CSS, REACT, JavaScript and creating back-end
+            connections, I am confident that I will fulfill the duties and
+            skills required for a developer role.
           </p>
         </div>
       </div>
