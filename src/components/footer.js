6cc67bb98e03e1f01f457ba075a37ff0ee7ad740
deannapi <deannapi.mart@gmail.com>
2020-12-01 14:54:58 -0600

    added badges and github links to portfolio

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,25 +10,47 @@ export default class Footer extends React.Component {
           <div className="contact-grid">
             <ul className="github">
               <li className="github">
-                <a href="https://github.com/deannapi">
+                <a
+                  href="https://github.com/deannapi"
+                  target="_blank"
+                  rel="noopener  noreferrer"
+                >
                   <i className="fab fa-github"></i>
                 </a>
               </li>
               <li className="stack-overflow">
                 <a href="https://stackoverflow.com/users/11610155/deanna-martinez">
                   <i>
-                    <img src={stack_ov} height="40px" alt="stackov" className="stackov"></img>
+                    <img
+                      src={stack_ov}
+                      height="40px"
+                      alt="stackov"
+                      className="stackov"
+                    ></img>
                   </i>
                 </a>
               </li>
               <li className="linkedin">
-                <a href="https://www.linkedin.com/in/deanna-martinez-63539a137/">
+                <a
+                  href="https://www.linkedin.com/in/deanna-martinez-63539a137/"
+                  target="_blank"
+                  rel="noopener  noreferrer"
+                >
                   <i className="fab fa-linkedin"></i>
                 </a>
               </li>
               <li className="harp">
-                <a href="https://www.harpbydeanna.com">
-                  <img className="harp-icon" src={harp_img} alt="harp" height="40px"></img>
+                <a
+                  href="https://www.harpbydeanna.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    className="harp-icon"
+                    src={harp_img}
+                    alt="harp"
+                    height="40px"
+                  ></img>
                 </a>
               </li>
             </ul>
--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -19,31 +19,80 @@ export default class Porfolio extends Component {
         <div className="projects">
           <ul>
             <li>
-              <a href="https://deannapi.github.io/password-generator/">
+              <a
+                href="https://deannapi.github.io/password-generator/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Password Generator
                 <br />
                 <img src={pswd_gen} alt="" height="175px" className="app-img" />
               </a>
               <p>Random password generator.</p>
+              <a
+                href="https://github.com/deannapi/password-generator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-primary">HTML</span>
+              <span className="badge badge-pill badge-secondary">CSS</span>
+              <span className="badge badge-pill badge-success">JavaScript</span>
             </li>
             <li>
-              <a href="https://deannapi.github.io/weather-app/">
+              <a
+                href="https://deannapi.github.io/weather-app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Weather App
                 <br />
                 <img src={weather} alt="" height="175px" className="app-img" />
               </a>
               <p>Find your daily weather and 5-day forecast.</p>
+              <a
+                href="https://github.com/deannapi/weather-app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-primary">HTML</span>
+              <span className="badge badge-pill badge-secondary">CSS</span>
+              <span className="badge badge-pill badge-success">JavaScript</span>
+              <span className="badge badge-pill badge-danger">jQuery</span>
+              <span className="badge badge-pill badge-info">API</span>
             </li>
             <li>
-              <a href="https://deannapi.github.io/work-day-scheduler/">
+              <a
+                href="https://deannapi.github.io/work-day-scheduler/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Work Day Scheduler
                 <br />
                 <img src={work_day} alt="" height="175px" className="app-img" />
               </a>
               <p>Schedule hourly tasks.</p>
+              <a
+                href="https://github.com/deannapi/work-day-scheduler"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-primary">HTML</span>
+              <span className="badge badge-pill badge-secondary">CSS</span>
+              <span className="badge badge-pill badge-success">JavaScript</span>
+              <span className="badge badge-pill badge-danger">jQuery</span>
             </li>
             <li>
-              <a href="https://ut-project-1-group-5.github.io/project-1-group-5/">
+              <a
+                href="https://ut-project-1-group-5.github.io/project-1-group-5/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Book and Movie Finder
                 <br />
                 <img
@@ -54,9 +103,24 @@ export default class Porfolio extends Component {
                 />
               </a>
               <p>Find a book and its' matching movie.</p>
+              <a
+                href="https://github.com/jpd61/project-1-group-5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-primary">HTML</span>
+              <span className="badge badge-pill badge-secondary">CSS</span>
+              <span className="badge badge-pill badge-success">JavaScript</span>
+              <span className="badge badge-pill badge-danger">jQuery</span>
             </li>
             <li>
-              <a href="https://deanna-note-taker.herokuapp.com/">
+              <a
+                href="https://deanna-note-taker.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Note Taker
                 <br />
                 <img
@@ -67,25 +131,69 @@ export default class Porfolio extends Component {
                 />
               </a>
               <p>Take notes & make reminders.</p>
+              <a
+                href="https://github.com/deannapi/note-taker"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">JSON</span>
+              <span className="badge badge-pill badge-light">Express.js</span>
+              <span className="badge badge-pill badge-warning">Node.js</span>
             </li>
             <li>
-              <a href="https://motor-services.herokuapp.com/">
+              <a
+                href="https://motor-services.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Motor Services
                 <br />
                 <img src={motor} alt="" height="175px" className="app-img" />
               </a>
               <p>Enter vehicle mileage for maintenance reminders.</p>
+              <a
+                href="https://github.com/deannapi/motor-services"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">MySQL</span>
+              <span className="badge badge-pill badge-dark">Handlebars.js</span>
+              <span className="badge badge-pill badge-light">Express.js</span>
+              <span className="badge badge-pill badge-warning">Node.js</span>
             </li>
             <li>
-              <a href="https://budgeting-tool-pwa.herokuapp.com/">
+              <a
+                href="https://budgeting-tool-pwa.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Budget Tracker
                 <br />
                 <img src={budget} alt="" height="175px" className="app-img" />
               </a>
               <p>Add deposits and withdrawals to your journal.</p>
+              <a
+                href="https://github.com/deannapi/budget-tracker"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">NoSQL</span>
+              <span className="badge badge-pill badge-dark">MongoDB</span>
+              <span className="badge badge-pill badge-light">Express.js</span>
+              <span className="badge badge-pill badge-warning">Node.js</span>
             </li>
             <li>
-              <a href="https://deannapi.github.io/food-festival/">
+              <a
+                href="https://deannapi.github.io/food-festival/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Food Festival
                 <br />
                 <img
@@ -96,9 +204,22 @@ export default class Porfolio extends Component {
                 />
               </a>
               <p>Find food at a local festival.</p>
+              <a
+                href="https://github.com/deannapi/food-festival/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-success">JavaScript</span>
+              <span className="badge badge-pill badge-warning">Node.js</span>
             </li>
             <li>
-              <a href="https://github.com/deannapi/book-search-engine">
+              <a
+                href="https://books-from-google.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Google Book Search
                 <br />
                 <img
@@ -109,9 +230,21 @@ export default class Porfolio extends Component {
                 />
               </a>
               <p>Search for books via Google.</p>
+              <a
+                href="https://github.com/deannapi/book-search-engine"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">MERN</span>
             </li>
             <li>
-              <a href="https://github.com/deannapi/deep-thoughts">
+              <a
+                href="https://github.com/deannapi/deep-thoughts"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Deep Thoughts
                 <br />
                 <img
@@ -122,19 +255,34 @@ export default class Porfolio extends Component {
                 />
               </a>
               <p>Social media application.</p>
+              <a
+                href="https://github.com/deannapi/deep-thoughts"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">MERN</span>
             </li>
             <li>
-              <a href="https://honey-do-list.herokuapp.com/">
+              <a
+                href="https://honey-do-list.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 Honey Do List
                 <br />
-                <img 
-                  src={honey_do}
-                  alt=""
-                  height="175px"
-                  className="app-img"
-                />
+                <img src={honey_do} alt="" height="175px" className="app-img" />
               </a>
               <p>Keep track of your chores and share them with family.</p>
+              <a
+                href="https://github.com/deannapi/honey-do-list"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i class="fab fa-github"></i>
+              </a>
+              <span className="badge badge-pill badge-dark">MERN</span>
             </li>
           </ul>
         </div>
