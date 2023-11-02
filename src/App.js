a6202fc8a391489b956d73bd6b9882a22ef6ae72
deannapi <deannapi.mart@gmail.com>
2023-11-02 09:28:12 -0500

    removed unsused components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { PageTransition } from "@steveeeie/react-page-transition";
 import Home from "./components/home";
 import Header from "./components/header";
-// import Resume from "./components/resume";
 import Portfolio from "./components/portfolio";
-// import Contact from "./components/contact";
 import Footer from "./components/footer";
 import "./App.css";
 
 const Links = () => (
   <>
     <Link to="/" className="links">Home</Link>
-    {/* <Link to="/resume" className="links">Resume</Link> */}
     <Link to="/portfolio" className="links">Portfolio</Link>
-    {/* <Link to="/contact" className="links">Contact</Link> */}
   </>
 );
 
@@ -33,9 +29,7 @@ export default function App() {
               <Switch location={location}>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/home" component={Home} />
-                {/* <Route exact path="/resume" component={Resume} /> */}
                 <Route exact path="/portfolio" component={Portfolio} />
-                {/* <Route exact path="/contact" component={Contact} /> */}
               </Switch>
             </PageTransition>
           );
